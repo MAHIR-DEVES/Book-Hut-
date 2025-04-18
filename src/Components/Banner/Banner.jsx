5aa042bc8f350fca01cd7f65dd60877e063658ad
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-04-18 14:53:05 +0600

    allmost done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import bookImage from '../../assets/books.jpg';
+import './banner.css';
 
 const Banner = () => {
   return (
-    <div className="flex">
-      <div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-        <button className="btn btn-primary">Expolr</button>
-      </div>
-      <div>
-        <img className="w-6/12" src={bookImage} alt="" />
+    <div className="flex justify-between items-center w-11/12 mx-auto">
+      <div className="banner flex items-center justify-between px-25">
+        <div className=" ">
+          <h1 className="text-4xl font-bold mb-4">
+            Explore endless stories, ignite your imagination, <br /> and find
+            the perfect book for <br />
+            every moment
+          </h1>
+          <button className="btn btn-primary text-xl">Explore More</button>
+        </div>
+        <div>
+          <img
+            className="max-w-md rounded-lg shadow-2xl"
+            src="https://i.ibb.co.com/rRPFN1rX/books.webp"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
